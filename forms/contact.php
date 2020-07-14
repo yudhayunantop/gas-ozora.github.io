ed3bd1bbafe958bb4e7692f6a751202dfb4e238e
yudhayunantop <yudha.yunanto.2010@gmail.com>
2020-07-14 18:46:27 +0700

    initial commit

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,12 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'gas-ozora.com',
+    'username' => 'info',
+    'password' => 'k+TY5hD[[Ltp',
+    'port' => '465'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
