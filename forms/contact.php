2cd95f86c5c2c60b28d7f48db01279c7c10fb205
Yudha Yunanto Putra <61044350+yudhayunantop@users.noreply.github.com>
2020-07-20 11:58:42 +0700

    coba email bismillah

--- forms/contact.php
+++ forms/contact.php
@@ -6,20 +6,22 @@ include('phpmailer/Exception.php');
 include('phpmailer/PHPMailer.php');
 include('phpmailer/SMTP.php');
 
+$email_pengirim = 'ozoragas@gmail.com'; // Isikan dengan email pengirim
+$nama_pengirim = $_POST['email'];
 $email_penerima = "info@gas-ozora.com";   // Ambil email penerima dari inputan form
-$email_pengirim = $_POST['email']; // Isikan dengan email pengirim
-$nama_pengirim = $_POST['name']; // Isikan dengan nama pengirim
+$nama_pengirim = $_POST['email']; // Isikan dengan nama pengirim
 $subjek = $_POST['subject']; // Ambil subjek dari inputan form
 $pesan = $_POST['message']; // Ambil pesan dari inputan form
 
-//$mail = new PHPMailer;
-//$mail->isSMTP();
-//$mail->Host = 'smtp.gmail.com';
-//$mail->Username = $email_pengirim; // Email Pengirim
-//$mail->Password = 'password_akun_email_pengirim'; // Isikan dengan Password email pengirim
-//$mail->Port = 465;
-//$mail->SMTPAuth = true;
-//$mail->SMTPSecure = 'ssl';
+$mail = new PHPMailer;
+$mail->isSMTP();
+
+$mail->Host = 'smtp.gmail.com';
+$mail->Username = $email_pengirim; // Email Pengirim
+$mail->Password = 'yudha30052000'; // Isikan dengan Password email pengirim
+$mail->Port = 465;
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
 
 // $mail->SMTPDebug = 2; // Aktifkan untuk melakukan debugging
 $mail->setFrom($email_pengirim, $nama_pengirim);
