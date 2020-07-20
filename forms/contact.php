6816e9a6cf728a5a96dd2a29a7638068d48eab75
yudhayunantop <yudha.yunanto.2010@gmail.com>
2020-07-20 14:31:18 +0700

    coba email pake phpmail

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@ include('phpmailer/PHPMailer.php');
 include('phpmailer/SMTP.php');
 
 $email_pengirim = 'ozoragas@gmail.com'; // Isikan dengan email pengirim
-$email_penerima = "info@gas-ozora.com";   // Ambil email penerima dari inputan form
+$email_penerima = 'info@gas-ozora.com';   // Ambil email penerima dari inputan form
 $nama_pengirim = $_POST['email']; // Isikan dengan nama pengirim
 $subjek = $_POST['subject']; // Ambil subjek dari inputan form
 $pesan = $_POST['message']; // Ambil pesan dari inputan form
@@ -22,14 +22,16 @@ $mail->Port = 465;
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'ssl';
 
+$mail->Body = $pesan;
+$mail->Subject = $subjek;
+
 // $mail->SMTPDebug = 2; // Aktifkan untuk melakukan debugging
 $mail->setFrom($email_pengirim, $nama_pengirim);
 $mail->addAddress($email_penerima, '');
-$send = $mail->send();
-
-if($send){ // Jika Email berhasil dikirim
-    header('Location: http://www.gas-ozora.com/');
-}else{ // Jika Email gagal dikirim
-    echo "<h1>Email gagal dikirim</h1>
-
+    $send = $mail->send();
+    if($send){ // Jika Email berhasil dikirim
+        header('Location: http://gas-ozora.com/');
+    }else{ // Jika Email gagal dikirim
+        echo "Mailer Error: " . $mail->ErrorInfo;
+    }
 ?>
